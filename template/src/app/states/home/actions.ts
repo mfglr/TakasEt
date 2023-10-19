7033c982433575cc238bb1dab033e017780ad649
mfglr <mfgglr@outlook.com>
2023-10-19 14:33:00 +0300

    delete home action file

--- template/src/app/states/home/actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-import { PostResponse } from "src/app/models/responses/post-response";
-
-export const getPosts = createAction("get posts")
-export const getPostsSuccess = createAction(
-  "get posts success",
-  props<{posts : PostResponse[]}>()
-)
