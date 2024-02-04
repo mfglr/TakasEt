1c22bca5c6b2bb03d233edef9fe25405066e1f37
Muhammed Furkan Güler <79373693+mfglr@users.noreply.github.com>
2024-02-04 20:02:32 +0300

    Update IRemovable.cs

--- SharedLibrary/SharedLibrary/Entities/IRemovable.cs
+++ SharedLibrary/SharedLibrary/Entities/IRemovable.cs
@@ -1,7 +1,7 @@
 ﻿namespace SharedLibrary.Entities
 {
 	/*
-	 * !!!The application can restore user data when users delete their accounts and want to recover them.!!!
+	 * !!!The application can restore user's data when users delete their accounts and want to recover them.!!!
 	 * 
 	 * When an entity is removed, the entity is not permanently deleted. It is just pointed as deleted.
 	 * 
